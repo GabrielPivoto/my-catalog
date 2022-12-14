381c65da1b8161dc3ed87502ed9ca818648e9f07
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-14 15:38:22 -0300

    :construction: Model

--- src/main/java/br/com/inatel/MyCatalog/model/dto/ShowSimpleDto.java
+++ src/main/java/br/com/inatel/MyCatalog/model/dto/ShowSimpleDto.java
@@ -1,5 +1,14 @@
 package br.com.inatel.MyCatalog.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShowSimpleDto {
 
     private String title;
--- src/main/java/br/com/inatel/MyCatalog/model/form/ShowForm.java
+++ src/main/java/br/com/inatel/MyCatalog/model/form/ShowForm.java
@@ -2,6 +2,7 @@ package br.com.inatel.MyCatalog.model.form;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ShowForm {
 
+    @NotEmpty
     private String title;
     @Min(0) @Max(10)
     private int personalScore;
