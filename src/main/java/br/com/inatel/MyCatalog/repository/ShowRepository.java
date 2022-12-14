36caeb66e4271b40cb6e21123fed1e09c597828a
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-14 15:39:19 -0300

    :construction: Repository

--- src/main/java/br/com/inatel/MyCatalog/repository/ShowRepository.java
+++ src/main/java/br/com/inatel/MyCatalog/repository/ShowRepository.java
@@ -1,12 +1,21 @@
 package br.com.inatel.MyCatalog.repository;
 
 import br.com.inatel.MyCatalog.model.entity.TvShow;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ShowRepository extends JpaRepository<TvShow, Integer> {
 
     Optional<TvShow> findByTitle(String title);
+
+    List<TvShow> findAllByType(@NotNull String type);
+
+//    @Query("SELECT t FROM TvShow t where t.type = :type")
+//    List<TvShow> findAllByType(@Param("type") String type);
 
 }
