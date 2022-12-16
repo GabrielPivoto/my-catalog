95756e11161bdf940fff9d2b06f6ad7f7cb17790
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-16 15:06:09 -0300

    :construction: Service

--- src/main/java/br/com/inatel/MyCatalog/service/ShowService.java
+++ src/main/java/br/com/inatel/MyCatalog/service/ShowService.java
@@ -1,7 +1,6 @@
 package br.com.inatel.MyCatalog.service;
 
 import br.com.inatel.MyCatalog.adapter.Adapter;
-import br.com.inatel.MyCatalog.exception.IncompatibleTypeException;
 import br.com.inatel.MyCatalog.exception.ShowAlreadyRegisteredException;
 import br.com.inatel.MyCatalog.exception.ShowNotFoundException;
 import br.com.inatel.MyCatalog.mapper.Mapper;
@@ -11,12 +10,10 @@ import br.com.inatel.MyCatalog.model.form.ShowForm;
 import br.com.inatel.MyCatalog.model.rest.Show;
 import br.com.inatel.MyCatalog.repository.ShowRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -25,18 +22,18 @@ public class ShowService {
     private ShowRepository showRepository;
     private Adapter adapter;
 
-    public ResponseEntity<ShowDto> addNewShow(ShowForm showForm){
-        String title = showForm.getTitle().toLowerCase().replace(" ","+");
-        Show show = adapter.getShowByTitle(title);
+    public ShowDto addNewShow(ShowForm showForm){
+        Show show = adapter.getShowByTitle(showForm.getTitle());
         Optional<TvShow> optional = showRepository.findByTitle(showForm.getTitle());
         if(show.getTitle() == null)
             throw new ShowNotFoundException("The show " + showForm.getTitle() + " could not be found." +
                     " Check if the name was written correctly.");
         else if(optional.isPresent())
-            throw new ShowAlreadyRegisteredException("The show " + showForm.getTitle() + " is already registered." +
-                    " Feel free to add another one.");
+            throw new ShowAlreadyRegisteredException("The show " + optional.get().getTitle() +
+                    " is already registered. Feel free to add another one.");
         else{
-            return ResponseEntity.created(null).body(Mapper.convertEntityToDto(showRepository.save(TvShow.builder()
+            return Mapper.convertEntityToDto(showRepository
+                    .save(TvShow.builder()
                     .personalScore(showForm.getPersonalScore())
                     .title(show.getTitle())
                     .rated(show.getRated())
@@ -47,71 +44,35 @@ public class ShowService {
                     .actors(show.getActors())
                     .plot(show.getPlot())
                     .imdbRating(show.getImdbRating())
-                    .type(show.getType()).build())));
+                    .type(show.getType()).build()));
         }
     }
 
-    public ResponseEntity<?> findShowByTitle(Optional<String> title){
-        if(title.isEmpty()){
-            List<TvShow> tvShows = showRepository.findAll();
-            return ResponseEntity.ok().body(tvShows.stream().map(Mapper::convertEntityToSimpleDto).collect(Collectors.toList()));
-        }else{
-            Optional<TvShow> optional = showRepository.findByTitle(title.get());
-            if(optional.isEmpty())
-                throw new ShowNotFoundException("The show " + title.get() + " could not be found." +
-                        " Check if the name was written correctly.");
-            return ResponseEntity.ok().body(Mapper.convertEntityToDto(optional.get()));
-        }
-    }
-
-    public ResponseEntity<List<ShowDto>> findAllShowsWithDetails(Optional<String> type){
+    public List<ShowDto> findShows(Optional<String> type){
         List<TvShow> tvShows;
-        if(type.isPresent())
+        if(type.isPresent()){
             tvShows = showRepository.findAllByType(type.get());
-        else
-            tvShows = showRepository.findAll();
-        return ResponseEntity.ok().body(tvShows.stream().map(Mapper::convertEntityToDto).collect(Collectors.toList()));
-    }
-
-
-    public ResponseEntity<List<ShowDto>> findAllShowsByType(String type){
-        List<TvShow> tvShows = showRepository.findAllByType(type);
-        return ResponseEntity.ok().body(tvShows.stream().map(Mapper::convertEntityToDto).collect(Collectors.toList()));
-    }
-
-    public ResponseEntity<?> findShows(Optional<String> title, Optional<String> type){
-        List<TvShow> tvShows;
-        if(title.isPresent()){
-            Optional<TvShow> optional = showRepository.findByTitle(title.get());
-            if(optional.isEmpty())
-                throw new ShowNotFoundException("The show " + title.get() + " could not be found." +
-                        " Check if the name was written correctly.");
-            else if(type.isPresent()){
-                if(!optional.get().getType().equals(type.get()))
-                    throw new IncompatibleTypeException("The show " + optional.get().getTitle() + " is not a " +
-                            type.get());
-            }
-            return ResponseEntity.ok().body(Mapper.convertEntityToDto(optional.get()));
-        }else if(type.isPresent()){
-            tvShows = showRepository.findAllByType(type.get());
-            return ResponseEntity.ok().body(tvShows.stream()
-                    .map(Mapper::convertEntityToDto).collect(Collectors.toList()));
-        }else{
-            tvShows = showRepository.findAll();
-            return ResponseEntity.ok().body(tvShows.stream()
-                    .map(Mapper::convertEntityToSimpleDto).collect(Collectors.toList()));
+            return tvShows.stream().map(Mapper::convertEntityToDto).toList();
         }
+        tvShows = showRepository.findAll();
+        return tvShows.stream().map(Mapper::convertEntityToDto).toList();
     }
 
-    public ResponseEntity<ShowDto> deleteShow(String title){
-        Optional<TvShow> optional = showRepository.findByTitle(title);
-        if(optional.isPresent()){
+    public ShowDto findShow(int id){
+        Optional<TvShow> optional = showRepository.findById(id);
+        if(optional.isEmpty())
+            throw new ShowNotFoundException("The id " + id + " could not be found." +
+                    " Try another one.");
+        return Mapper.convertEntityToDto(optional.get());
+    }
+
+    public void deleteShow(int id){
+        Optional<TvShow> optional = showRepository.findById(id);
+        if(optional.isPresent())
             showRepository.delete(optional.get());
-            return ResponseEntity.noContent().build();
-        }
-        throw new ShowNotFoundException("The show " + title + " could not be found." +
-                " Check if the name was written correctly.");
+        else
+            throw new ShowNotFoundException("The id " + id + " could not be found." +
+                    " Try another one.");
     }
-
 
 }
