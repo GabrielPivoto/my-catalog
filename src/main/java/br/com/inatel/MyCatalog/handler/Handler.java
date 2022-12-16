f1cc5253f9a02258740c6e2bbca66bd41a2f2500
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-16 15:05:35 -0300

    :construction: Handler

--- src/main/java/br/com/inatel/MyCatalog/handler/Handler.java
+++ src/main/java/br/com/inatel/MyCatalog/handler/Handler.java
@@ -5,9 +5,11 @@ import br.com.inatel.MyCatalog.exception.ShowAlreadyRegisteredException;
 import br.com.inatel.MyCatalog.exception.ShowNotFoundException;
 import br.com.inatel.MyCatalog.model.dto.ErrorDto;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,10 +17,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@AllArgsConstructor
 public class Handler {
 
     private MessageSource messageSource;
+
+    @Value("${container.name}")
+    private String instance;
+
+    public Handler(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
+
 
     @ExceptionHandler(ShowNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
@@ -28,19 +38,19 @@ public class Handler {
                 .title("Show not found or doesn't exist.")
                 .httpStatus(HttpStatus.NOT_FOUND)
                 .detail(e.getMessage())
-                .instance("192.168.1.106")
+                .instance(instance)
                 .build();
     }
 
     @ExceptionHandler(ShowAlreadyRegisteredException.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
     public ErrorDto generateException(ShowAlreadyRegisteredException e){
         return ErrorDto.builder()
                 .type("ShowAlreadyRegisteredException")
                 .title("Show already registered.")
-                .httpStatus(HttpStatus.BAD_REQUEST)
+                .httpStatus(HttpStatus.CONFLICT)
                 .detail(e.getMessage())
-                .instance("192.168.1.106")
+                .instance(instance)
                 .build();
     }
 
@@ -54,7 +64,7 @@ public class Handler {
                 .title("Invalid personal score")
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .detail("Personal score " + message)
-                .instance("192.168.1.106")
+                .instance(instance)
                 .build();
     }
 
@@ -66,7 +76,19 @@ public class Handler {
                 .title("Incompatible types")
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .detail(e.getMessage())
-                .instance("192.168.1.106")
+                .instance(instance)
+                .build();
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorDto generateException(HttpMessageNotReadableException e){
+        return ErrorDto.builder()
+                .type("HttpMessageNotReadableException")
+                .title("Invalid personal score")
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .detail("Personal score must be a double.")
+                .instance(instance)
                 .build();
     }
 
