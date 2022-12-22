c628c63957ffb8aa2a69a21f8f29167276b1bae5
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-22 08:44:37 -0300

    :construction: Config

---
+++ src/main/java/br/com/inatel/MyCatalog/config/SwaggerConfiguration.java
@@ -0,0 +1,35 @@
+package br.com.inatel.MyCatalog.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "springdoc.swagger-ui.enabled", havingValue = "true", matchIfMissing = true)
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(info());
+    }
+
+    private Info info() {
+        String title = "MyCatalog API";
+        String description = "App to register movies and series";
+        String version = "1.0";
+        String contactName = "Gabriel Pivoto";
+        String contactEmail = "gabriel.pivoto@inatel.br";
+        String contactUrl = "https://gabrielpivoto.github.io/";
+        return new Info()
+                .title(title)
+                .description(description)
+                .version(version)
+                .contact(new Contact().name(contactName).email(contactEmail).url(contactUrl));
+    }
+
+}
