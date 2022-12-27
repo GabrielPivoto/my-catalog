edb581332641643779c625a7f29fe9a1febd8b12
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-27 09:46:24 -0300

    :construction: Mapper

--- src/main/java/br/com/inatel/MyCatalog/mapper/Mapper.java
+++ src/main/java/br/com/inatel/MyCatalog/mapper/Mapper.java
@@ -1,10 +1,7 @@
 package br.com.inatel.MyCatalog.mapper;
 
 import br.com.inatel.MyCatalog.model.dto.ShowDto;
-import br.com.inatel.MyCatalog.model.dto.ShowSimpleDto;
 import br.com.inatel.MyCatalog.model.entity.TvShow;
-import br.com.inatel.MyCatalog.model.form.ShowForm;
-import br.com.inatel.MyCatalog.model.rest.Show;
 
 public class Mapper {
 
@@ -25,27 +22,4 @@ public class Mapper {
                 .build();
     }
 
-    public static ShowSimpleDto convertEntityToSimpleDto(TvShow tvShow){
-        return ShowSimpleDto.builder()
-                .title(tvShow.getTitle())
-                .id(tvShow.getId())
-                .personalScore(tvShow.getPersonalScore())
-                .build();
-    }
-
-    public static ShowDto convertRestToDto(Show show, ShowForm showForm) {
-        return ShowDto.builder()
-                .actors(show.getActors())
-                .personalScore(showForm.getPersonalScore())
-                .director(show.getDirector())
-                .title(show.getTitle())
-                .genre(show.getGenre())
-                .imdbRating(show.getImdbRating())
-                .plot(show.getPlot())
-                .rated(show.getRated())
-                .released(show.getReleased())
-                .type(show.getType())
-                .writer(show.getWriter())
-                .build();
-    }
 }
