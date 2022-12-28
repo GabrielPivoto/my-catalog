2e36d936345c3396c233982d8d28551190785eda
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-12-28 11:52:51 -0300

    :construction: Adding request filter

---
+++ src/main/java/br/com/inatel/MyCatalog/filter/TransactionFilter.java
@@ -0,0 +1,38 @@
+package br.com.inatel.MyCatalog.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+public class TransactionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        ContentCachingRequestWrapper req = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper resp = new ContentCachingResponseWrapper(response);
+
+        filterChain.doFilter(req, resp);
+
+        byte[] requestBody = req.getContentAsByteArray();
+        byte[] responseBody = resp.getContentAsByteArray();
+
+        log.info("request body = {}", new String(requestBody, StandardCharsets.UTF_8));
+
+        log.info("response body = {}", new String(responseBody, StandardCharsets.UTF_8));
+
+        resp.copyBodyToResponse();
+    }
+
+}
